8ebf659c0fd021f7c0275c812878d0f81aae228a
Lindseytaub <lindseytaub@Lindseys-MacBook-Air.local>
2014-08-12 22:21:15 -0700

    remove relationships

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -1,6 +1,3 @@
 class Song < ActiveRecord::Base
 
-  belongs_to_many :songs
- belongs_to_many :singers, through: :songs
-
 end
--- app/models/singer.rb
+++ app/models/singer.rb
@@ -1,6 +1,3 @@
 class Singer < ActiveRecord::Base
 
- has_many :songs
-  has_many :lyrics, through: :songs
-
 end
